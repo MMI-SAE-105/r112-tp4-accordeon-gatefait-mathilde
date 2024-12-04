4cb0f3f7b7063d37640a786948e848a88f7d3cf6
gatefait-mathilde <mathilde.gatefait@edu.univ-fcomte.fr>
2024-12-04 23:27:20 +0100

    essaie js + transition

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 // *** ACCORDEON ***
+
+const accordeon = document.querySelector(".accordeon");
+
+accordeon.addEventListener("click", (event) => {
+    if (event.target.tagName === "SUMMARY") {
+        accordeon.querySelectorAll("details").forEach((det) => {
+            if (det !== event.target.parentElement) {
+                det.open = false;
+            }
+        });
+    }
+});
+
+
+
+
+
